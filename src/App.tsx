c9fb9454c2f105d7ca06ad6f6e99e1eae102f128
Mehirion <Mehirion1@gmail.com>
2024-11-27 23:17:45 +0100

    Poprawka przed merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,83 @@
-import React from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import React from 'react';
+import { uploadData, isCancelError} from 'aws-amplify/storage';
+import { FileUploader } from '@aws-amplify/ui-react-storage';
 
-const client = generateClient<Schema>();
+import '@aws-amplify/ui-react/styles.css';
+
+
+export const DefaultFileUploaderExample = () => {
+  return (
+    <FileUploader
+      acceptedFileTypes={['image/*']}
+      path="public/"
+      maxFileCount={1}
+      isResumable
+    />
+  );
+};
+
+try {
+  const result = await uploadData({
+    path: "album/2024/1.jpg",
+    // Alternatively, path: ({identityId}) => `album/${identityId}/1.jpg`
+    data: file,
+    options: {
+      bucket: 'firstBucket'
+    }
+  }).result;
+  console.log('Succeeded: ', result);
+} catch (error) {
+  console.log('Error : ', error);
+}
+
+const monitorUpload = async () => {
+  try {
+    const result = await uploadData({
+      path: "album/2024/1.jpg",
+      // Alternatively, path: ({identityId}) => `album/${identityId}/1.jpg`
+      data: file,
+      options: {
+        onProgress: ({ transferredBytes, totalBytes }) => {
+          if (totalBytes) {
+            console.log(
+              `Upload progress ${Math.round(
+                (transferredBytes / totalBytes) * 100
+              )} %`
+            );
+          }
+        },
+      },
+    }).result;
+    console.log("Path from Response: ", result.path);
+  } catch (error) {
+    console.log("Error : ", error);
+  }
+  
+}
+
+
 
 function App() {
-  const [file, setFile] = React.useState<File | undefined>();
+  const [file, setFile] = React.useState();
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
+  const handleChange = (event: any) => {
       setFile(event.target.files[0]);
-    }
-  };
-
-  const handleUpload = async () => {
-    if (!file) {
-      alert("Please select a file to upload");
-      return;
-    }
-
-    try {
-      await client.uploadData({
-        path: `picture-submissions/${file.name}`,
-        data: file,
-      });
-      alert("File uploaded successfully!");
-    } catch (error) {
-      console.error("Upload failed", error);
-      alert("File upload failed. Please try again.");
-    }
   };
 
   return (
-    <div>
-      <input type="file" onChange={handleChange} />
-      <button onClick={handleUpload}>Upload</button>
-    </div>
+      <div>
+          <input type="file" onChange={handleChange} />
+          <button
+              onClick={() =>
+                  uploadData({
+                      path: `photos/${file.name}`,
+                      data: file,
+                  })
+              }
+          >
+              Upload
+          </button>
+      </div>
   );
 }
 
